bfe8e1d338015a740b4dce915da01cc4e13667cd
Alexander Makarof <makarofalex@gmail.com>
2019-05-17 13:06:34 +0300

    continue add edit match fields

--- src/Components/Admin/AdminMatches/addEditMatch.js
+++ src/Components/Admin/AdminMatches/addEditMatch.js
@@ -44,6 +44,87 @@ export default class AddEditMatch extends Component {
 				validationMessage: '',
 				showlabel: false
 			},
+			resultLocal: {
+				element: 'input',
+				value: '',
+				config: {
+					label: 'Event date',
+					name: 'result_local_input',
+					type: 'date'
+				},
+				validation: {
+					required: true
+
+				},
+				valid: false,
+				validationMessage: '',
+				showlabel: false
+			},
+			away: {
+				element: 'select',
+				value: '',
+				config: {
+					label: 'select a local team',
+					name: 'select_local',
+					type: 'select',
+					options: []
+				},
+				validation: {
+					required: true
+
+				},
+				valid: false,
+				validationMessage: '',
+				showlabel: false
+			},
+			resultAway: {
+				element: 'input',
+				value: '',
+				config: {
+					label: 'Event date',
+					name: 'result_local_input',
+					type: 'date'
+				},
+				validation: {
+					required: true
+
+				},
+				valid: false,
+				validationMessage: '',
+				showlabel: false
+			},
+			referee: {
+				element: 'input',
+				value: '',
+				config: {
+					label: 'Referee',
+					name: 'referee_input',
+					type: 'text'
+				},
+				validation: {
+					required: true
+
+				},
+				valid: false,
+				validationMessage: '',
+				showlabel: true
+			},
+			stadium: {
+				element: 'input',
+				value: '',
+				config: {
+					label: 'Stadium',
+					name: 'stadium_input',
+					type: 'text'
+				},
+				validation: {
+					required: true
+
+				},
+				valid: false,
+				validationMessage: '',
+				showlabel: true
+			},
 		}
 	}
 	render() {
@@ -59,6 +140,41 @@ export default class AddEditMatch extends Component {
 					id='date'
 					formdata={this.state.formdata.date}
 					change={(element) => this.updateForm(element)} />
+
+				<div className="select_team_layout">
+					<div className="label_inputs">	Local</div>
+					<div className="wrapper">
+						<div className="left">
+							<FormField
+								id='local'
+								formdata={this.state.formdata.local}
+								change={(element) => this.updateForm(element)} /></div>
+						<div>
+							<FormField
+								id='reusltLocal'
+								formdata={this.state.formdata.resultLocal}
+								change={(element) => this.updateForm(element)} />
+						</div>
+					</div>
+				</div>
+
+				<div className="select_team_layout">
+					<div className="label_inputs">	Away</div>
+					<div className="wrapper">
+						<div className="left">
+							<FormField
+								id='away'
+								formdata={this.state.formdata.away}
+								change={(element) => this.updateForm(element)} /></div>
+						<div>
+							<FormField
+								id='reusltAway'
+								formdata={this.state.formdata.resultAway}
+								change={(element) => this.updateForm(element)} />
+						</div>
+					</div>
+				</div>
+
 			</AdminLayout>
 		)
 	}
--- src/Components/ui/formField.js
+++ src/Components/ui/formField.js
@@ -19,7 +19,7 @@ const FormField = ({ formdata, id, change }) => {
 							<div className='label_inputs'>
 								{formdata.config.label}
 							</div>
-							: ''}
+							: null}
 						<input
 							{...formdata.config}
 							value={formdata.value}
@@ -28,7 +28,31 @@ const FormField = ({ formdata, id, change }) => {
 					</div>
 				)
 				break;
-
+			case 'select':
+				formTemplate = (
+					<div>
+						{formdata.showlabel ?
+							<div className='label_inputs'>
+								{formdata.config.label}
+							</div>
+							: null}
+						<select
+							value={formdata.value}
+							onChange={(event) => change({ event, id })}	>
+							<option value=''>
+								Select one
+							</option>
+							{formdata.config.options.map((item) => (
+								<option
+									key={item.key}
+									value={item.key}>
+									{item.value}
+								</option>
+							))}
+						</select>
+					</div>
+				)
+				break;
 			default:
 				formTemplate = null;
 		}
