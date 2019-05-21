226aec7e3a26d0372cce85c177aa71cd1f22edcc
Alexander Makarof <makarofalex@gmail.com>
2019-05-21 21:17:15 +0300

    whas known to getdownloadurl was deprecated =)

--- src/Components/TheTeam/TheTeam.js
+++ src/Components/TheTeam/TheTeam.js
@@ -4,7 +4,7 @@ import Fade from 'react-reveal/Fade';
 import stripes from '../../Resources/images/stripes.png';
 import { firebasePlayers, firebase } from './../../firebase';
 import { firebaseLooper } from './../ui/misc';
-import { Promise } from 'core-js';
+// import { Promise } from 'core-js';
 
 
 export default class TheTeam extends Component {
@@ -13,31 +13,36 @@ export default class TheTeam extends Component {
 		players: [],
 	}
 	componentDidMount() {
-		firebasePlayers
-			.once('value')
-			.then(snapshot => {
-				const players = firebaseLooper(snapshot);
-				let promises = [];
-				console.log(players);
-				for (let key in players) {
-					promises.push(
-						new Promise((resolve, reject) => {
-							firebase.storage().ref('players')
-								.child(players[key].image).getDownloadURL()
-								.then(url => {
-									players[key].url = url;
-									resolve();
-								})
-						})
-					)
-				}
-				Promise.all(promises).then(() => {
-					this.setState({
-						loading: false,
-						players
+		firebasePlayers.once('value').then(snapshot => {
+			const players = firebaseLooper(snapshot);
+			let promises = [];
+			let some = firebase.storage().ref('players').child(players[0].image);
+			console.log(some);
+			// some.getDownloadURL();
+
+
+
+			for (let key in players) {
+				promises.push(
+					new Promise((resolve, reject) => {
+						let some = firebase.storage().ref('players').child(players[key].image);
+
+						some.getDownloadURL()
+							.getDownloadURL()
+							.then(url => {
+								players[key].url = url;
+								resolve();
+							})
 					})
+				)
+			}
+			Promise.all(promises).then(() => {
+				this.setState({
+					loading: false,
+					players
 				})
 			})
+		})
 	}
 
 	render() {
